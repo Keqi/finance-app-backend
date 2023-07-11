92c61fcf689502422d9e8bf2c44fa18e16ddc4b1
Maciej Nowak <maciej.nowak90@gmail.com>
2023-07-11 17:12:54 +0200

    Chore: Adds cors module

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const financeRecords = require('./../queries/finance_records')
 
+app.use(cors())
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
